9c85762586d85996934a5c796461a476c3e2b46c
ajoedhi19 <74102918+ajoedhi19@users.noreply.github.com>
2021-10-05 15:02:37 +0700

    Finalize tugas

---
+++ Tugas 1/main.js
@@ -0,0 +1,78 @@
+let speed = 0.0125;
+let dy = 0;
+
+function main() {
+  var canvas = document.getElementById("myCanvas");
+  var gl = canvas.getContext("webgl");
+
+  var vertices = [...vertices_kiri, ...vertices_kanan];
+
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  var vertexShaderSource = `
+        attribute vec2 aPosition;
+        attribute vec3 aColor;
+        varying  vec3 vColor;
+        uniform mat4 uTranslate;
+        void main(){
+            gl_Position = uTranslate * vec4(aPosition, 0.0, 1.0);
+            vColor = aColor;
+        }
+    `;
+
+  var fragmentShaderSource = `
+        precision mediump float;
+        varying vec3 vColor;
+        void main(){
+            gl_FragColor = vec4(vColor, 1.0);
+        }
+    `;
+
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexShaderSource);
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentShaderSource);
+
+  gl.compileShader(vertexShader);
+  gl.compileShader(fragmentShader);
+
+  var shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
+
+  var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+  gl.enableVertexAttribArray(aPosition);
+
+  var aColor = gl.getAttribLocation(shaderProgram, "aColor");
+  gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+  gl.enableVertexAttribArray(aColor);
+
+  const uTranslate = gl.getUniformLocation(shaderProgram, "uTranslate");
+
+  function render() {
+    if (dy >= 0.7 || dy <= -0.46) speed = -speed;
+    dy += speed;
+
+    const kiri = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0, 0.0, 0.0, 1.0];
+
+    const kanan = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, dy, 0.0, 1.0];
+
+    gl.clearColor(0.8, 0.8, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.uniformMatrix4fv(uTranslate, false, kiri);
+    gl.drawArrays(gl.TRIANGLES, 0, vertices_kiri.length / 5);
+
+    gl.uniformMatrix4fv(uTranslate, false, kanan);
+    gl.drawArrays(gl.TRIANGLES, vertices_kiri.length / 5, vertices_kanan.length / 5);
+
+    requestAnimationFrame(render);
+  }
+  render();
+}
